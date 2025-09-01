96fa569f8e25cd58b64c01fb32e2976b69ec7e3d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-01 11:41:48 -0300

    subindo codigo da aula05 concatenar, somar e conversão de valores

--- App.tsx
+++ App.tsx
@@ -1,45 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from "react";
+import {Text, TextInput, Button, View, StyleSheet} from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+const App = () => {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [resultado, setResultado] = useState<number|null>(null);
+  const [erro, setErro] = useState<string>('');
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const somarDoisNum = () => {
 
+    //converte os valores para número
+    const n1 = parseFloat(num1);
+    const n2 = parseFloat(num2);
+
+    if (isNaN(n1) || isNaN(n2)){
+      setErro('Por favor, insira números válidos');
+      setResultado(null);
+    }else{
+      const soma = n1+n2;
+      setResultado(soma);
+      setErro('');
+    }
+  };
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+    <View style= {styles.container}>
+      <View style={styles.inputConatiner}>
+        <Text style={styles.label}>Número 1:</Text>
+        <TextInput style={styles.input} keyboardType="numeric" value={num1} onChangeText={setNum1}/>
+      </View>
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+      <View style={styles.inputConatiner}>
+        <Text style={styles.label}>Número 2:</Text>
+        <TextInput style={styles.input} keyboardType="numeric" value={num2} onChangeText={setNum2}/>
+      </View>
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Button title="Somar" onPress={somarDoisNum}/>
+
+      {erro ? <Text style={styles.error}>{erro}</Text>:null}
+
+      {resultado !== null && (
+        <Text style={styles.resultado}>Resultado: {resultado}</Text>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#f5f5f5',
   },
+  inputConatiner:{
+    marginBottom:20,
+  },
+  label:{
+    fontSize:18,
+    marginBottom:5,
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: '#888',
+    borderWidth: 1,
+    paddingLeft:10,
+    fontSize:16,
+  },
+  resultado: {
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  error:{
+    marginTop:20,
+    color:'red',
+  }
 });
-
 export default App;
+ 
